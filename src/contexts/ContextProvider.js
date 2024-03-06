8bab804368e506651b76eed30936b27b8b0f49cc
Mark Pradilla <markpradilla9@gmail.com>
2024-03-06 11:57:42 +0800

    Admin Dashboar React

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const InitialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(InitialState);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...InitialState, [clicked]: true });
+  };
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
